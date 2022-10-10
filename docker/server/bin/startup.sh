676cd443980c4adb0dfd972869631fe892737e25
Yuyi Pacheco K <yuyik46@gmail.com>
2022-10-10 14:06:38 -0400

    feat: setup es7 support

--- docker/server/bin/startup.sh
+++ docker/server/bin/startup.sh
@@ -1,36 +1,35 @@
 #!/bin/sh
-#
-#  Copyright 2021 Netflix, Inc.
-#  <p>
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
-#  the License. You may obtain a copy of the License at
-#  <p>
-#  http://www.apache.org/licenses/LICENSE-2.0
-#  <p>
-#  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
-#  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
-#  specific language governing permissions and limitations under the License.
-#
 
-# startup.sh - startup script for the server docker image
-
-echo "Starting Conductor server"
-
-# Start the server
-cd /app/libs
-echo "Property file: $CONFIG_PROP"
-echo $CONFIG_PROP
-export config_file=
-
-if [ -z "$CONFIG_PROP" ];
-  then
-    echo "Using an in-memory instance of conductor";
-    export config_file=/app/config/config-local.properties
-  else
-    echo "Using '$CONFIG_PROP'";
-    export config_file=/app/config/$CONFIG_PROP
+# Install AWS CLI
+if which aws > /dev/null
+then
+  echo "AWS CLI is installed, skipping..."
+else
+  echo "Install AWS CLI"
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  ./aws/install
 fi
 
-echo "Using java options config: $JAVA_OPTS"
+# Setting up configuration
+echo "Setting up configuration file..."
+cd /app/libs
+echo "Property file: $CONFIG_PROP"
+echo "Remote property file: $REMOTE_CONFIG_PROP"
+export config_file=
 
+if [ -n $REMOTE_CONFIG_PROP ]; then
+  echo "Downloading remote config file..."
+  aws s3 cp $REMOTE_CONFIG_PROP /app/config/config-remote.properties
+  export config_file=/app/config/config-remote.properties
+elif [ -n "$CONFIG_PROP" ]; then
+  echo "Using config file..."
+  export config_file=/app/config/$CONFIG_PROP
+else
+  echo "Using an in-memory instance of conductor";
+  export config_file=/app/config/config-local.properties
+fi
+
+# Run Conductor
+echo "Using java options config: $JAVA_OPTS"
 java ${JAVA_OPTS} -jar -DCONDUCTOR_CONFIG_FILE=$config_file conductor-server-*-boot.jar 2>&1 | tee -a /app/logs/server.log
