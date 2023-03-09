720145d0b1e816c2c8c2318a614c0cb506f9410b
Yuyi Pacheco K <yuyik46@gmail.com>
2023-03-09 00:39:32 -0400

    feat: setup github actions

--- docker/server/bin/startup.sh
+++ docker/server/bin/startup.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-# Install AWS CLI
-if which aws > /dev/null
-then
-  echo "AWS CLI is installed, skipping..."
-else
-  echo "Install AWS CLI"
-  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-  unzip awscliv2.zip
-  ./aws/install
-fi
-
 # Setting up configuration
 echo "Setting up configuration file..."
 cd /app/libs
@@ -20,7 +9,7 @@ export config_file=
 
 if [ -n "${REMOTE_CONFIG_PROP}" ]; then
   echo "Downloading remote config file..."
-  aws s3 cp $REMOTE_CONFIG_PROP /app/config/config-remote.properties
+  gcloud storage cp $REMOTE_CONFIG_PROP /app/config/config-remote.properties
   export config_file=/app/config/config-remote.properties
 elif [ -n "${CONFIG_PROP}" ]; then
   echo "Using config file..."
